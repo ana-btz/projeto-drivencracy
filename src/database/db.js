6ead91b202f7ec54c23829017cba67e995afa3fb
ana_btz <silva.anabtz@gmail.com>
2023-05-03 19:35:32 -0300

    feat: conectar ao mongodb

---
+++ src/database/db.js
@@ -0,0 +1,17 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+try {
+  await mongoClient.connect();
+  console.log("Conectado com Mongodb");
+} catch (error) {
+  console.log(error);
+}
+export const db = mongoClient.db();
+
+// export const pollsCollection = db.collection("polls");
+// export const choicesCollection = db.collection("choices");
+// export const votesCollection = db.collection("votes");
--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
