ac45fce78ac0a8da544c0d4fc030f153e81ab9a4
ana_btz <silva.anabtz@gmail.com>
2023-05-03 20:08:14 -0300

    feat: deploy mongodb atlas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import cors from "cors";
+import { db } from "./database/db.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
