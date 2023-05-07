e32dc8f4b698360fe681a127de1cf7779ca28336
ana_btz <silva.anabtz@gmail.com>
2023-05-07 10:46:49 -0300

    feat: implement GET request for /poll/:id/choice route

--- src/controllers/choice.controllers.js
+++ src/controllers/choice.controllers.js
@@ -2,7 +2,7 @@ import dayjs from "dayjs";
 import { choicesCollection, pollsCollection } from "../database/db.js";
 import { ObjectId } from "mongodb";
 
-export async function createChoice(req, res) {
+export async function createPollChoice(req, res) {
   const { title, pollId } = req.body;
 
   try {
@@ -19,6 +19,20 @@ export async function createChoice(req, res) {
     await choicesCollection.insertOne({ title, pollId });
 
     res.sendStatus(201);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function getPollChoices(req, res) {
+  const { id } = req.params;
+  console.log(id);
+
+  try {
+    const pollChoices = await choicesCollection.find({ pollId: id }).toArray();
+    if (!pollChoices) return res.sendStatus(404);
+
+    res.status(200).send(pollChoices);
   } catch (error) {
     res.status(500).send(error.message);
   }
--- src/routes/choice.routes.js
+++ src/routes/choice.routes.js
@@ -1,10 +1,14 @@
-import { Router } from "express";
-import { createChoice } from "../controllers/choice.controllers.js";
-import { validateSchema } from "../middlewares/validate.schema.middleware.js";
 import { choiceSchema } from "../schemas/choice.schema.js";
+import { Router } from "express";
+import { validateSchema } from "../middlewares/validate.schema.middleware.js";
+import {
+  createPollChoice,
+  getPollChoices,
+} from "../controllers/choice.controllers.js";
 
 const choiceRouter = Router();
 
-choiceRouter.post("/choice", validateSchema(choiceSchema), createChoice);
+choiceRouter.post("/choice", validateSchema(choiceSchema), createPollChoice);
+choiceRouter.get("/poll/:id/choice", getPollChoices);
 
 export default choiceRouter;
